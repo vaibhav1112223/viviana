5973f08f9f37f6fc9985dfcf9f1b631910a67289
Vaibhav Yadav <vaibhavy284@gmail.com>
2025-10-23 17:11:02 +0530

    Update

--- viviana-backend/server.js
+++ viviana-backend/server.js
@@ -33,10 +33,10 @@ app.use("/api/auth", authRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../viviana-frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../viviana-frontend/build/index.html"));
 });
 
 
